79ac109f64b41fbce15eaf89d611e0de935dd3d8
KalaiarasanK <52280933+KalaiarasanK@users.noreply.github.com>
2019-07-16 14:13:36 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,9 +1,9 @@
-A,B=list(map(str,input().split()))
+S1,S2=list(map(str,input().split()))
 c=0
-for i in range (0,len(A)):
-	if A[i]!=B[i]:
+for i in range (0,len(S1)):
+	if S1[i]!=S2[i]:
 		c+=1
 if(c==1):
-	print("Yes")
+	print("yes")
 else:
-	print("No")
+	print("no")
